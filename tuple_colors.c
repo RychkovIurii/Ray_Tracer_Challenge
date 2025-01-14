ff4d8df13295f514c135467aea7e5993492df0d2
Rychkov Iurii <irychkov@student.hive.fi>
2025-01-14 15:23:21 +0200

    Renamed multiply_color to hadamard_product.

--- tuple_colors.c
+++ tuple_colors.c
@@ -6,7 +6,7 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 15:06:59 by irychkov          #+#    #+#             */
-/*   Updated: 2025/01/14 15:16:07 by irychkov         ###   ########.fr       */
+/*   Updated: 2025/01/14 15:21:51 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,16 @@ t_tuple	color(double r, double g, double b)
 	return (tuple);
 }
 
-t_tuple	multiply_color(t_tuple a, t_tuple b)
+/* Hadamard product is a term for element-wise multiplication of two tuples.
+We can call it a "multiply_color" as well. */
+
+t_tuple	hadamard_product(t_tuple a, t_tuple b)
 {
 	t_tuple	tuple;
 
 	tuple.x = a.x * b.x;
 	tuple.y = a.y * b.y;
 	tuple.z = a.z * b.z;
-	tuple.w = 0.0;
+	tuple.w = a.w * b.w; // Think what to do with the flag.
 	return (tuple);
 }
