0771881e92bd17c9a87ecad731f280b3138df573
Rychkov Iurii <irychkov@student.hive.fi>
2025-01-17 14:15:49 +0200

    Clock challenge. A program that uses a rotation matrix to compute the positions of those hours on the clock face, and draw a pixel onto a canvas for each of them.

--- main.c
+++ main.c
@@ -5,69 +5,64 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/14 19:13:34 by irychkov          #+#    #+#             */
-/*   Updated: 2025/01/14 19:35:58 by irychkov         ###   ########.fr       */
+/*   Created: 2025/01/17 12:34:27 by irychkov          #+#    #+#             */
+/*   Updated: 2025/01/17 14:14:51 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "tuple.h"
 #include "canvas.h"
+#include "matrix.h"
 
-typedef struct s_projectile
-{
-	t_tuple	position;
-	t_tuple	velocity;
-}	t_projectile;
+#define WIDTH 1000
+#define HEIGHT 1000
+#define RADIUS (WIDTH * 3 / 8)  // Clock's radius is 3/8 of canvas width
+#define CENTER_X (WIDTH / 2)
+#define CENTER_Y (HEIGHT / 2)
 
-typedef struct s_environment
-{
-	t_tuple	gravity;
-	t_tuple	wind;
-}	t_environment;
-
-t_projectile	tick(t_environment env, t_projectile proj)
-{
-	t_projectile	new_proj;
-
-	new_proj.position = add_tuple(proj.position, proj.velocity);
-	new_proj.velocity = add_tuple(add_tuple(proj.velocity, env.gravity), env.wind);
-	return (new_proj);
-}
+/* Challenge is to write a program that uses a rotation matrix to compute 
+the positions of those hours on the clock face, and draw a pixel onto 
+a canvas for each of them. */
 
 int main()
 {
-	t_projectile	proj;
-	t_environment	env;
 	t_canvas	*canvas;
-	t_tuple		color;
+	t_tuple		black, white;
+	t_matrix	rotation;
+	t_tuple		p;
 	int			i;
 	int			j;
 
-	proj.position = point(0, 1, 0);
-	proj.velocity = normalize(vector(1, 1.8, 0));
-	proj.velocity = multiply_tuple_scalar(proj.velocity, 11.25);
-	env.gravity = vector(0, -0.1, 0);
-	env.wind = vector(-0.01, 0, 0);
-
-	canvas = create_canvas(900, 550);
-	color = create_color(0, 0, 0);
-	for (i = 0; i < 900; i++)
+	canvas = create_canvas(WIDTH, HEIGHT);
+	black = create_color(0, 0, 0);
+	for (i = 0; i < WIDTH; i++)
 	{
-		for (j = 0; j < 550; j++)
+		for (j = 0; j < HEIGHT; j++)
 		{
-			write_pixel(canvas, i, j, color);
+			write_pixel(canvas, i, j, black);
 		}
 	}
-	while (proj.position.y > 0)
+	white = create_color(1, 1, 1);
+	rotation = rotation_y_matrix(M_PI / 6);
+	p = point(0, 0, 1);
+	for (i = 0; i < 12; i++)
 	{
-		int x = (int)proj.position.x;
-        int y = (int)(550 - proj.position.y); // Convert to canvas coordinates (y-axis flipped)
-        t_tuple trajectory_color = create_color(1, 0, 0);
-        if (x >= 0 && x < 900 && y >= 0 && y < 550) { // Check canvas bounds
-            write_pixel(canvas, x, y, trajectory_color);
-        }
-		proj = tick(env, proj);
+		// Scale and translate to canvas coordinates
+		int x = (int)(p.x * RADIUS + CENTER_X);
+		int y = (int)(p.z * RADIUS + CENTER_Y);
+
+		// Ensure the point is within canvas bounds
+		if (x >= 0 && x < WIDTH && y >= 0 && y < HEIGHT) {
+			write_pixel(canvas, x, y, white);
+			printf("Pixel at (%d, %d)\n", x, y);
+		}
+		p = multiply_matrix_by_tuple(rotation, p);
+		/* int x = (int)(p.x * 100 + 500);
+		int y = (int)(p.z * 100 + 500);
+		if (x >= 0 && x < 1000 && y >= 0 && y < 1000) {  // Boundary check
+			write_pixel(canvas, x, y, white);
+		} */
 	}
-	canvas_to_ppm(canvas, "red1.ppm");
+	canvas_to_ppm(canvas, "clock.ppm");
 	return (0);
 }
