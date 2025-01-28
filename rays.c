6d1c730ddc6b66abe3fbcf15cb64bf1c7d13a3d8
Iurii Rychkov <rychkoviurii@hotmail.com>
2025-01-28 14:49:08 +0200

    Found an approach for intersections. I will keep it for now. Intersect keeps array of intersections.

--- rays.c
+++ rays.c
@@ -6,11 +6,26 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 11:30:27 by irychkov          #+#    #+#             */
-/*   Updated: 2025/01/28 12:12:52 by irychkov         ###   ########.fr       */
+/*   Updated: 2025/01/28 14:47:36 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "tuple.h"
+
+typedef enum e_object_type
+{
+	t_sphere,
+	t_plane,
+	t_cylinder,
+	t_cone,
+	t_triangle
+}				t_object_type;
+
+typedef struct	s_object
+{
+	int			type;
+	void		*data; // should think about this
+}				t_object;
 
 typedef struct	s_ray
 {
@@ -24,16 +39,17 @@ typedef struct	s_sphere
 	double		radius;
 }				t_sphere;
 
-typedef struct	s_sphere_intersection
-{
-	double	t1;
-	double	t2;
-}				t_sphere_intersection;
+typedef struct	s_intersect // array of intersections
 
-typedef struct	s_intersection
+{
+	int					count;
+	t_intersection		*array;
+}						t_intersect;
+
+typedef struct	s_intersection // t value and object
 {
 	double		t;
-	t_sphere	sphere;
+	t_object	*object;
 }				t_intersection;
 
 t_ray			create_ray(t_tuple origin, t_tuple direction)
@@ -53,6 +69,16 @@ t_tuple			get_ray_position(t_ray ray, double t)
 	return (result);
 }
 
+
+/* t_intersection create_intersection(double t, t_object *object)
+{
+	t_intersection result;
+
+	result.t = t;
+	result.object = object;
+	return (result);
+} */
+
 /*
 	Function reurns the number of intersections between the ray and the sphere
 	and the values of t at which the intersections occur.
@@ -60,16 +86,16 @@ t_tuple			get_ray_position(t_ray ray, double t)
 	Hit is a point where the ray intersects the sphere.
 	We need all values for reflections and refractions.
 */
-t_sphere_intersection intersect_sphere(t_ray ray, t_sphere sphere)
+t_intersect	intersect_sphere(t_ray ray, t_sphere sphere)
 {
-	t_sphere_intersection result;
+	t_intersect result;
 	t_tuple		sphere_to_ray;
 	double		a;
 	double		b;
 	double		c;
 	double		discriminant;
-	double		t1;
-	double		t2;
+	t_intersection	intersection1;
+	t_intersection	intersection2;
 
 	sphere_to_ray = substract_tuple(ray.origin, sphere.center);
 	a = dot(ray.direction, ray.direction);
@@ -79,30 +105,17 @@ t_sphere_intersection intersect_sphere(t_ray ray, t_sphere sphere)
 
 	if (discriminant < 0)
 	{
-		memset(&result, 0, sizeof(t_sphere_intersection));
+		result.count = 0;
+		result.array = NULL;
+		return (result);
 	}
-	else
-	{
-		result.t1 = (-b - sqrt(discriminant)) / (2 * a);
-		result.t2 = (-b + sqrt(discriminant)) / (2 * a);
-		if (result.t1 > result.t2)
-		{
-			double temp = result.t1;
-			result.t1 = result.t2;
-			result.t2 = temp;
-		}
-	}
+	intersection1.t = (-b - sqrt(discriminant)) / (2 * a);
+	intersection1.object = &sphere;
+	intersection2.t = (-b + sqrt(discriminant)) / (2 * a);
+	intersection2.object = &sphere;
+	result.count = 2;
+	result.array = (t_intersection *)malloc(sizeof(t_intersection) * 2);
+	result.array[0] = intersection1;
+	result.array[1] = intersection2;
 	return (result);
-}
-
-double hit(t_sphere_intersection intersections)
-{
-	if (intersections.count == 0)
-		return (-1);
-	if (intersections.count == 1)
-		return (intersections.t1);
-	if (intersections.t1 < intersections.t2)
-		return (intersections.t1);
-	else
-		return (intersections.t2);
 }
