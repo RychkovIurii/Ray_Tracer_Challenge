0f0c1e8276b409bdfe761c22a302fed4fcc83302
Iurii Rychkov <rychkoviurii@hotmail.com>
2025-02-03 21:07:16 +0200

    Added ray_fro_pixel to the camera.c. Tested.

--- camera.c
+++ camera.c
@@ -6,7 +6,7 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 19:09:13 by irychkov          #+#    #+#             */
-/*   Updated: 2025/02/03 19:23:33 by irychkov         ###   ########.fr       */
+/*   Updated: 2025/02/03 20:32:20 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,8 +51,23 @@ t_camera create_camera(int hsize, int vsize, double field_of_view)
 	return (camera);
 }
 
+t_ray ray_for_pixel(t_camera camera, int px, int py)
+{
+	double xoffset = (px + 0.5) * camera.pixel_size;
+	double yoffset = (py + 0.5) * camera.pixel_size;
 
-/* #include <assert.h>
+	double world_x = camera.half_width - xoffset;
+	double world_y = camera.half_height - yoffset;
+
+	t_tuple pixel = multiply_matrix_by_tuple(inverse_matrix(camera.transform), point(world_x, world_y, -1));
+	t_tuple origin = multiply_matrix_by_tuple(inverse_matrix(camera.transform), point(0, 0, 0));
+	t_tuple direction = normalize(substract_tuple(pixel, origin));
+
+	return create_ray(origin, direction);
+}
+
+/* 
+#include <assert.h>
 
 void test_pixel_size_horizontal_canvas()
 {
@@ -68,9 +83,41 @@ void test_pixel_size_vertical_canvas()
 	printf("test_pixel_size_vertical_canvas passed\n");
 }
 
+void test_ray_for_pixel_center()
+{
+	t_camera c = create_camera(201, 101, M_PI / 2);
+	t_ray r = ray_for_pixel(c, 100, 50);
+	assert(is_tuples_equal(r.origin, point(0, 0, 0)));
+	assert(is_tuples_equal(r.direction, vector(0, 0, -1)));
+	printf("test_ray_for_pixel_center passed\n");
+}
+
+void test_ray_for_pixel_corner()
+{
+	t_camera c = create_camera(201, 101, M_PI / 2);
+	t_ray r = ray_for_pixel(c, 0, 0);
+	assert(is_tuples_equal(r.origin, point(0, 0, 0)));
+	assert(is_tuples_equal(r.direction, vector(0.66519, 0.33259, -0.66851)));
+	printf("test_ray_for_pixel_corner passed\n");
+}
+
+void test_ray_for_pixel_transformed()
+{
+	t_camera c = create_camera(201, 101, M_PI / 2);
+	c.transform = multiply_matrices(rotation_y_matrix(M_PI / 4), translation_matrix(0, -2, 5));
+	t_ray r = ray_for_pixel(c, 100, 50);
+	assert(is_tuples_equal(r.origin, point(0, 2, -5)));
+	assert(is_tuples_equal(r.direction, vector(sqrt(2)/2, 0, -sqrt(2)/2)));
+	printf("test_ray_for_pixel_transformed passed\n");
+}
+
 int main()
 {
 	test_pixel_size_horizontal_canvas();
 	test_pixel_size_vertical_canvas();
+	test_ray_for_pixel_center();
+	test_ray_for_pixel_corner();
+	test_ray_for_pixel_transformed();
 	return 0;
-} */
+}
+ */
